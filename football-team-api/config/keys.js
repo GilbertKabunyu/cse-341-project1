be65102f60d85b5a79f2681c844ba81f45765f47
GilbertKabunyu <gkabunyuk15@gmail.com>
2024-10-16 23:01:14 +0200

    removed football api from this contact api

--- football-team-api/config/keys.js
+++
@@ -1,7 +0,0 @@
-// config/keys.js
-require('dotenv').config();
-
-module.exports = {
-  mongoURI: process.env.MONGODB_URI || 'mongodb://localhost:27017/football-api',
-  secretOrKey: process.env.JWT_SECRET || 'me' 
-};
